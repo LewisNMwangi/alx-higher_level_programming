c93ebb8a4122aa3bd49954b36f2378c67055b4c6
LewisNMwangi <lewisnjuguna8@gmail.com>
2022-10-25 17:23:23 +0300

    Task 14. Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3ads from standard input and computes metrics.
 
 """
 After every ten lines or the input of a keyboard interruption (CTRL + C),
@@ -10,6 +10,7 @@ prints the following statistics:
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
+
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
