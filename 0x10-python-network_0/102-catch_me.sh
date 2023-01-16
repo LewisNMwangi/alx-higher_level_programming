398a83d9b432c0bebff711f559474764c024701e
LewisNMwangi <lewisnjuguna8@gmail.com>
2023-01-16 21:57:56 +0300

    Task 9. Catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -X GET -s 0.0.0.0:5000/catch_me | grep -q "You got me!" && echo "You got me!".
+response=$(curl -X GET 0.0.0.0:5000/catch_me)
+if [[ $response == *"You got me!"* ]]; then
+echo "You got me!"
+fi
+
